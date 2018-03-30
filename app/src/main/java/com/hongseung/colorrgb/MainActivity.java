68ac8fdb5d1b2cd99595e16fe3416ffe6e8c3b6b
Seunghyun.hong <hong.seung.hyun.23@gmail.com>
2018-03-30 18:07:51 +0900

    널체크, 255 조정 했음. 그리고 색상코드 나오게 함.

--- app/src/main/java/com/hongseung/colorrgb/MainActivity.java
+++ app/src/main/java/com/hongseung/colorrgb/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +25,6 @@ public class MainActivity extends AppCompatActivity {
         mColorGreenEditText = findViewById(R.id.color_green_edit_text);
         mColorBlueEditText = findViewById(R.id.color_blue_edit_text);
 
-         Button
-        mColorButton11 = findViewById(R.id.color_button_11);
 
     }
 
@@ -40,14 +39,73 @@ public class MainActivity extends AppCompatActivity {
     public void ColorPrint(int view) {
         Button button = findViewById(view);
 
-        String stringR = mColorRedEditText.getText().toString();
-        String stringG = mColorGreenEditText.getText().toString();
-        String stringB = mColorBlueEditText.getText().toString();
+        //255 넘는 수 그리고 널체크 필요!
+        String stringR;
+        String stringG;
+        String stringB;
+
+        //R 널체크
+        if (mColorRedEditText.getText().length() == 0) {
+            stringR = "0";
+            Toast.makeText(this, "빈칸은 0으로 대체되었습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            stringR = mColorRedEditText.getText().toString();
+        }
+
+        //G 널체크
+        if (mColorGreenEditText.getText().length() == 0) {
+            stringG = "0";
+            Toast.makeText(this, "빈칸은 0으로 대체되었습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            stringG = mColorGreenEditText.getText().toString();
+        }
+
+        //B 널체크
+        if (mColorBlueEditText.getText().length() == 0) {
+            stringB = "0";
+            Toast.makeText(this, "빈칸은 0으로 대체되었습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            stringB = mColorBlueEditText.getText().toString();
+        }
+
 
         int intR = Integer.parseInt(stringR);
         int intG = Integer.parseInt(stringG);
         int intB = Integer.parseInt(stringB);
 
-        button.setBackgroundColor(Color.rgb(intR,intG,intB));
+        // 255 한계체크
+        if (intR > 255) {
+            intR = 255;
+            Toast.makeText(this, "255초과되는 값을 적어 255로 조정하였습니다.", Toast.LENGTH_SHORT).show();
+        } else {
+            intR = Integer.parseInt(stringR);
+        }
+
+        if (intG > 255) {
+            intG = 255;
+            Toast.makeText(this, "255초과되는 값을 적어 255로 조정하였습니다.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            intG = Integer.parseInt(stringG);
+        }
+
+        if (intB > 255) {
+            intB = 255;
+            Toast.makeText(this, "255초과되는 값을 적어 255로 조정하였습니다.", Toast.LENGTH_SHORT).show();
+
+        } else {
+            intB = Integer.parseInt(stringB);
+        }
+
+
+        int intColor = Color.rgb(intR, intG, intB);
+
+        // 컬러 int 값을 16진수로 변환하고, 포멧도 바꿔줌
+        String hexColor = String.format("#%06X", (0xFFFFFF & intColor));
+
+        button.setBackgroundColor(intColor); // 배경색을 바꿈
+//        button.setText(""+intColor);
+
+        button.setText(hexColor);
     }
 }
